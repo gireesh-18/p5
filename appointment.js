4c046c2e1c8f700df4031c158c797527caf2719d
gireesh-18 <98466305+gireesh-18@users.noreply.github.com>
2022-11-03 18:19:33 +0530

    Add via files upload

---
+++ appointment.js
@@ -0,0 +1,29 @@
+let conf = new URLSearchParams(location.search);
+            const name = conf.get('name')
+            const phone = conf.get('phone')
+            const file_case = conf.get('file_case')
+            const date = conf.get('date')
+            const slot = conf.get('slot')
+           
+            document.getElementById("name").innerHTML = name === "" ? "xyz" : name;
+            document.getElementById("phone").innerHTML = phone === "" ? "1234567890" : phone;
+            document.getElementById("case").innerHTML = file_case === "" ? "Newcase" : file_case;
+            document.getElementById("date").innerHTML = date === "" ? "01/01/2021" : date;
+            document.getElementById("time").innerHTML = slot === "" ? "5PM - 5:30PM" : slot;
+
+            function sendMail(params){
+                emailjs.send("service_u8rx2xh","template_r8ey57d",{
+                patient_name: name,
+                patient_number: phone,
+                case: file_case,
+                appointment_date: date,
+                time_slot: slot,
+                })
+            
+            swal({
+                    title: "Appointment Status",
+                    text: "SUCCESS! Your Appointment has been successfully booked. Please reach the hospital within the appointed time. To check waiting status, contact +91 9408218912",
+                    button: "Exit"
+                });
+                
+        }
